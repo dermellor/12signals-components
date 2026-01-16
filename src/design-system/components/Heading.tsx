8eb6df7d0133b9c1e0e6688c114bb5850ae33cda
Marcel Mellor <info@mellors.de>
2026-01-16 13:19:46 +0100

    fix: heading d.ts build

--- src/design-system/components/Heading.tsx
+++ src/design-system/components/Heading.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 
 type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 type HeadingProps = {
   level?: HeadingLevel;
 } & React.HTMLAttributes<HTMLHeadingElement>;
 
 export function Heading({ level = 2, className, children, ...rest }: HeadingProps) {
-  const Comp = `h${level}` as keyof JSX.IntrinsicElements;
+  const Comp = `h${level}` as HeadingTag;
   const cn = ["ds-Heading", className].filter(Boolean).join(" ");
   return (
     <Comp className={cn} data-level={level} {...rest}>
